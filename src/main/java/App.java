163a9062658a6d1f31280b2120aa3a1fc4c090c6
serhiimischenko <serj.roshen@gmail.com>
2022-12-19 18:42:33 +0200

    Added new Gradle project and create fat jar myname.jar

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        Map<String, String> user = new HashMap<>();
+        user.put("lastName", "Mischenko");
+        user.put("name", "Serhii");
+        System.out.println(new Gson().toJson(user));
+    }
+}
